d861ecd686730243ce623c0c902c06ec779a656a
princewayne18 <pdube1@byupathway.edu>
2025-05-14 09:41:07 +0200

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        string number = Console.ReadLine();
+        int magicNumber = int.Parse(number);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your Guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
+            
+        }
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+        guess = -1;
+
+
+
+         while (guess != randomNumber)
+        {
+            Console.Write("What is your Guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
+        }
+
     }
 }
