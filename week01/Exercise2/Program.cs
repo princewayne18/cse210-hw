009530f241f4311692318c8239d9aed4187c6312
princewayne18 <pdube1@byupathway.edu>
2025-05-14 08:35:02 +0200

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradeNumber = int.Parse(userInput);
+
+        if (gradeNumber>=90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (gradeNumber>=80)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if (gradeNumber>=70)
+        {
+            Console.WriteLine("C");
+
+        }
+        else if(gradeNumber>=60)
+        {
+            Console.WriteLine("D");
+
+        }
+        else
+        {
+           Console.WriteLine("F"); 
+        }
+        
+    if (gradeNumber>=70)
+    {
+        Console.WriteLine("Cogradulation you passed your exams!!!!!!");
+    }
+    else
+    {
+        Console.WriteLine("Unfortunately you failed your exam, try again next time");
+    }
     }
 }
