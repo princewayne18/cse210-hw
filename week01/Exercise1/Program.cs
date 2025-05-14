6b53ff2e1029739d1d09846d9bc3eebfb9f484f3
princewayne18 <pdube1@byupathway.edu>
2025-05-14 07:47:26 +0200

    exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.Write($"Your name is {last}, {name } {last}.");
     }
 }
