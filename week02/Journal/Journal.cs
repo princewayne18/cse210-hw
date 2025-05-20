f8784e608c1eaaa9a83de5205e3913301dfbae10
princewayne18 <pdube1@byupathway.edu>
2025-05-20 08:11:07 +0200

    final journal project

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,25 +1,56 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
 public class Journal
 {
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>();
 
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
+
 
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
-    public void SaveToFILE(string file)
+    public void SaveToFile(string file)
     {
-
+    
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText} {entry._mood}");
+            }
+        }
     }
+
+
+
 
     public void LoadFromFile(string file)
     {
         
+        string[] lines = System.IO.File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            Entry entry = new Entry
+            {
+                _date = parts[0],
+                _promptText = parts[1],
+                _entryText = parts[2],
+                _mood=parts[3]
+            };
+            _entries.Add(entry);
+        }
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal myJournal = new Journal();
+        PromptGenerator promptGen = new PromptGenerator();
+
+        while (true)
+        {
+            Console.WriteLine("Wlecome to the Journal Program!");
+            Console.WriteLine("Please Select one one of the following choice:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = promptGen.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    Console.Write("> ");
+                    string response = Console.ReadLine();
+                    Console.Write("How are you feeling today? (e.g., Happy, Sad, Anxious): ");
+                    string mood = Console.ReadLine();
+
+                    Entry newEntry = new Entry
+                    {
+                        _date = DateTime.Now.ToString("yyyy-MM-dd"),
+                        _promptText = prompt,
+                        _entryText = response,
+                        _mood = mood
+                    };
+                    myJournal.AddEntry(newEntry);
+                    break;
+
+                case "2":
+                    myJournal.DisplayAll();
+                    break;
+
+                case "3":
+                    Console.Write("Enter filename to save to: ");
+                    string saveFile = Console.ReadLine();
+                    myJournal.SaveToFile(saveFile);
+                    Console.WriteLine("Journal saved.\n");
+                    break;
+
+                case "4":
+                    Console.Write("Enter filename to load from: ");
+                    string loadFile = Console.ReadLine();
+                    myJournal.LoadFromFile(loadFile);
+                    Console.WriteLine("Journal loaded.\n");
+                    break;
+
+                case "5":
+                    Console.WriteLine("Goodbye!");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select again.\n");
+                    break;
+            }
+             
+       
+        }
     }
 }
